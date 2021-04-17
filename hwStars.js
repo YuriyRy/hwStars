fdab96da992f37a624db69e2f16bc986dd917e1a
Yuriy <riabukhayurii@gmail.com>
2021-04-17 03:16:25 +0300

    Starss

--- hwStars.js
+++ hwStars.js
@@ -21,92 +21,144 @@ var star = '*';
 var space = ' ';
 
 for (var i = 0; i < vertLines; i++) {
-    var str = '';
+  var str = '';
 
- 
-    if (i == 0 || i == vertLines - 1) {    
-      for (var j = 0; j < topLines; j++) {
-        str += star;
-      }    
 
-    } else {
-      str = star;
-      for (var j = 0; j < topLines - 2 ; j++) {
-        str += space;
-      }    
+  if (i == 0 || i == vertLines - 1) {
+    for (var j = 0; j < topLines; j++) {
       str += star;
     }
-    console.log(str);    
+
+  } else {
+    str = star;
+    for (var j = 0; j < topLines - 2; j++) {
+      str += space;
+    }
+    str += star;
+  }
+  console.log(str);
 }
 
-function triangleFirst () {
+function triangleFirst() {
   var elem = ""
   for (var i = 0, d = 6; i < 7; i++, d--) {
-      for (var k = 0; k < 7; k++) {
-          if (i == 0) {
-              elem += "*  ";
-          } else if (k == 0 || k == d) { 
-              elem += "*  "
-          } else {
-              elem += "   "
-          }
+    for (var k = 0; k < 7; k++) {
+      if (i == 0) {
+        elem += "*  ";
+      } else if (k == 0 || k == d) {
+        elem += "*  "
+      } else {
+        elem += "   "
       }
-      elem += "\n";
+    }
+    elem += "\n";
   }
   return elem;
 }
 console.log(triangleFirst());
 
-function triangleSec () {
+function triangleSec() {
   var elem = ""
   for (var i = 0, d = 0; i < 7; i++, d++) {
-      for (var k = 0; k < 7; k++) {
-          if (i == 6) {
-              elem += "*  ";
-          } else if (k == 0 || k == d) { 
-              elem += "*  "
-          } else {
-              elem += "   "
-          }
+    for (var k = 0; k < 7; k++) {
+      if (i == 6) {
+        elem += "*  ";
+      } else if (k == 0 || k == d) {
+        elem += "*  "
+      } else {
+        elem += "   "
       }
-      elem += "\n";
+    }
+    elem += "\n";
   }
   return elem;
 }
 console.log(triangleSec());
 
-function triangleThird () {
+function triangleThird() {
   var elem = ""
   for (var i = 7, d = 6; i > 0; i--, d--) {
-      for (var k = 0; k < 7; k++) {
-          if (i == 1) {
-              elem += "*  ";
-          } else if (k == 6 || k == d) { 
-              elem += "*  "
-          } else {
-              elem += "   "
-          }
+    for (var k = 0; k < 7; k++) {
+      if (i == 1) {
+        elem += "*  ";
+      } else if (k == 6 || k == d) {
+        elem += "*  "
+      } else {
+        elem += "   "
       }
-      elem += "\n";
+    }
+    elem += "\n";
   }
   return elem;
 }
 console.log(triangleThird());
 
-function triangleFourth () {
+function triangleFourth() {
   var elem = ""
   for (var i = 0, d = 0; i < 7; i++, d++) {
-      for (var k = 0; k < 7; k++) {
-          if (i == 0) {
-              elem += "*  ";
-          } else if (k == 6 || k == d) { 
-              elem += "*  "
-          } else {
-              elem += "   "
-          }
+    for (var k = 0; k < 7; k++) {
+      if (i == 0) {
+        elem += "*  ";
+      } else if (k == 6 || k == d) {
+        elem += "*  "
+      } else {
+        elem += "   "
       }
-      elem += "\n";
+    }
+    elem += "\n";
   }
   return elem;
 }
 console.log(triangleFourth());
+
+function cross() {
+  var star = '';
+  for (var i = 0; i < 7; i++) {
+    for (var j = 0; j < 7; j++) {
+      if (j === i || i === j) {
+        star += '*  ';
+      } else {
+        star += '   ';
+      }
+    }
+    star += '\n';
+  }
+  return star;
+}
+console.log(cross());
+
+
+
+function star8() {
+  var star = '';
+  for (var i = 1; i < 8; i++) {
+    for (var j = 7, k = 1; j >= 1, k < 8; j--, k++) {
+      if (i === 1 || i === j && j < 5 ||  k === i && k < 5 ) {
+        star += '* ';
+      } else {
+        star += '  ';
+      }
+    }
+    star += "\n";
+  }
+  return star;
+}
+
+console.log(star8());
+
+function star9() {
+  var star = '';
+  for (var i = 1; i < 8; i++) {
+    for (var j = 7, k = 1; j >= 1, k < 8; j--, k++) {
+      if (i === 7 || i === j && j > 3 ||  k === i && k > 3 ) {
+        star += '* ';
+      } else {
+        star += '  ';
+      }
+    }
+    star += "\n";
+  }
+  return star;
+}
+
+console.log(star9());
